2d0b0582013f0c9a65d57ac1733cd3d77f709674
HarryNguyen1712 <nahuy@vnptit3.vn>
2022-07-21 12:20:15 +0700

    update create (category)

--- src/main/java/com/vnpt/intership/news/api/v1/repository/CategoriesRepository.java
+++ src/main/java/com/vnpt/intership/news/api/v1/repository/CategoriesRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoriesRepository extends MongoRepository<CategoriesEntity, ObjectId> {
+    CategoriesEntity findByCategoryName(String name);
 }
